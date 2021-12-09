5d338802fa577452e06e441b1b9790652fb3a2a9
MadaShindeInai <sergei.nepryahin.work@gmail.com>
2021-12-09 18:34:14 +0100

    feat: fix swagger

--- src/server.ts
+++ src/server.ts
@@ -1,37 +1,43 @@
-import fastify from 'fastify';
-// import path from 'path';
+import fastify, { FastifyInstance } from 'fastify';
+import { Server, IncomingMessage, ServerResponse } from 'http';
+import helmet from 'fastify-helmet';
+import fastifySwagger from 'fastify-swagger';
+import fastifyFormbody from 'fastify-formbody';
+import path from 'path';
 import { PORT } from './common/config';
 import users from './resources/users/user.router';
 import boards from './resources/boards/board.router';
 import tasks from './resources/tasks/task.router';
 
-const fastifyServer = fastify();
+const app: FastifyInstance<Server, IncomingMessage, ServerResponse> = fastify();
+app.register(helmet);
+app.register(fastifyFormbody);
 
 // TODO: reslve problem with swagger
-// fastifyServer.register(require('fastify-swagger'), {
-//   mode: 'static',
-//   specification: {
-//     path: path.join(__dirname, '../doc/api.yaml'),
-//   },
-//   exposeRoute: true,
+app.register(fastifySwagger, {
+  mode: 'static',
+  specification: {
+    path: path.join(__dirname, '../doc/api.yaml'),
+    baseDir: '/src',
+  },
+  exposeRoute: true,
+  routePrefix: '/doc',
+});
 
-//   routePrefix: '/doc',
-// });
+app.ready((err) => {
+  if (err) throw err;
+  app.swagger();
+});
 
-// fastifyServer.ready((err) => {
-//   if (err) throw err;
-//   fastifyServer.swagger();
-// });
-
-fastifyServer.register(users);
-fastifyServer.register(boards);
-fastifyServer.register(tasks);
+app.register(users);
+app.register(boards);
+app.register(tasks);
 
 const startServer = async () => {
   try {
-    await fastifyServer.listen(PORT);
+    await app.listen(PORT);
   } catch (err) {
-    fastifyServer.log.error(err);
+    app.log.error(err);
     // eslint-disable-next-line no-process-exit
     process.exit(1);
   }
