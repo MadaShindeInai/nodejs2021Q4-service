359a1c886c49a3d567a2ff888ff91dbde6663709
MadaShindeInai <sergei.nepryahin.work@gmail.com>
2022-01-22 21:40:54 +0100

    feat: add description in terminal, fix migrations

--- src/common/migrations/1642879949992-addAdminUser.ts
+++ src/common/migrations/1642879949992-addAdminUser.ts
@@ -3,7 +3,7 @@ import { MigrationInterface, QueryRunner } from 'typeorm';
 export class addAdminUser1642879949992 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      `INSERT INTO "user" VALUES('a2193b13-af16-4f13-9f9c-0b082f1c14e1', 'admin', 'admin', 'admin');`
+      `INSERT INTO "user" (id, "name", login, "password") VALUES('a2193b13-af16-4f13-9f9c-0b082f1c14e1', 'admin', 'admin', '$2b$10$ENenM33Q6ufPAfrydJCsp.irP15rYnMgYzDFiBc8ZEyrvfFQjyRE2');`
     );
   }
 
